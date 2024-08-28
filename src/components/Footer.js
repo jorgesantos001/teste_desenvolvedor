ff4b6eb85d2ab6c7e01c1276c141de525360e5f0
jorgesantos001 <patrociniojorgeluiz@gmail.com>
2024-08-28 12:05:31 -0300

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>Contato: contato@lojavirtual.com</p>
+    <p>
+      Redes sociais: 
+      <a href="https://facebook.com/lojavirtual">Facebook</a> | 
+      <a href="https://twitter.com/lojavirtual">Twitter</a>
+    </p>
+    <p>&copy; 2024 Loja Virtual. Todos os direitos reservados.</p>
+  </footer>
+);
+
+export default Footer;
